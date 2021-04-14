849427bc2b774410c9c638493ed6bab52a801907
Anders Rune Jensen <arj03@protonmail.ch>
2021-04-15 00:40:25 +0200

    Rewrite to be callback based and not use atomically

--- index.js
+++ index.js
@@ -1,39 +1,4 @@
 if (typeof localStorage === "undefined" || localStorage === null)
-{
-  module.exports = require('atomically')
-}
+  module.exports = require("./fs")
 else
-{
-  const IdbKvStore = require('idb-kv-store')
-  const promisify = require('promisify-4loc')
-
-  function getStoreAndKey(filename) {
-    const parts = filename.split('/')
-    const key = parts.pop()
-    const storename = parts.join('/')
-    const store = new IdbKvStore(storename, { disableBroadcast: true })
-    return { store, key }
-  }
-
-  function isUint8Array(value) {
-    return toString.call(value).indexOf('Uint8Array') !== -1
-  }
-
-  module.exports = {
-    readFile: function(filename) {
-      const { store, key } = getStoreAndKey(filename)
-      const storeGet = store.get.bind(store)
-      function get(key, cb) {
-        storeGet(key, (err, value) => {
-          if (err) return cb(err)
-          else cb(null, isUint8Array(value) ? Buffer.from(value) : value)
-        })
-      }
-      return promisify(get)(key)
-    },
-    writeFile: function(filename, value) {
-      const { store, key } = getStoreAndKey(filename)
-      return promisify(store.set.bind(store))(key, value)
-    }
-  }
-}
+  module.exports = require("./browser")
--- test/basic.js
+++ test/basic.js
@@ -1,17 +1,16 @@
 const tape = require('tape')
 const { readFile, writeFile } = require('../')
 
-const greetings = Buffer.from('GREETINGS')
-
 tape('Basic', (t) => {
-  writeFile("test.txt", greetings).then(x => {
-    readFile("test.txt").then(buf => {
+  const greetings = Buffer.from('GREETINGS')
+  writeFile("test.txt", greetings, (err, x) => {
+    readFile("test.txt", (err, buf) => {
       t.deepEqual(buf, greetings)
       
       const greetingsStr = 'Greetings!'
       
-      writeFile("test2.txt", greetingsStr, { encoding: 'utf8' }).then(x => {
-        readFile("test2.txt", { encoding: 'utf8' }).then(str => {
+      writeFile("test2.txt", greetingsStr, { encoding: 'utf8' }, (err, x) => {
+        readFile("test2.txt", { encoding: 'utf8' }, (err, str) => {
           t.deepEqual(str, greetingsStr)
           t.end()
         })
